895fb346bf1e1a1dbebc40df18f10ee479a93893
DR-WOW <490514283@qq.com>
2025-02-26 21:57:37 +0800

    Update multi-model-RF-ET.py

--- multi-model-RF-ET.py
+++ multi-model-RF-ET.py
@@ -7,12 +7,12 @@ import streamlit.components.v1 as components
 
 # Load the models
 random_forest = joblib.load('Random Forest.pkl')
-#extra_trees = joblib.load('Extra Trees.pkl')
+extra_trees = joblib.load('Extra Trees.pkl')
 
 # Model dictionary
 models = {
     'Random Forest (RF)': random_forest,
-    #'Extra Trees': extra_trees
+    'Extra Trees': extra_trees
 }
 
 # Title
